e68c0feec5d8ca6b809bf6ef39c3b3210c6ef32c
MrBs <Chouaibwidadipur@gmail.com>
2023-09-13 17:01:28 +0100

    Added the rest of functions + betty

--- shell.h
+++ shell.h
@@ -15,6 +15,8 @@ char *readline(void);
 int _strlen(char *string);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
 
 
 #endif
--- strings.c
+++ strings.c
@@ -63,3 +63,52 @@ char *_strcpy(char *dest, char *src)
 	*dest = '\0';
 	return (dest_start);
 }
+
+/**
+ * _strdup - Duplicate a string
+ * @str: The string to duplicate
+ * Returns duplicated string, or NULL
+ */
+
+char *_strdup(const char *str)
+{
+	char *ptr;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str = str - len;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+		for (i = 0; i <= len; i++)
+			ptr[i] = str[i];
+			return (ptr);
+}
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ * Returns: An integer value representing the difference between `s1` and `s2`
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int cmp;
+
+	cmp = (int)*s1 - (int)*s2;
+	while (*s2)
+	{
+		if (*s1 != *s2)
+			break;
+			s1++;
+			cmp = (int)*s1 - (int)*s2;
+	}
+	return (cmp);
+}
