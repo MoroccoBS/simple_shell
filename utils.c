08198a66ca5357042da673dd8a1d17f82f41bc30
aminefaouzi39 <126355359+aminefaouzi39@users.noreply.github.com>
2023-09-15 16:28:17 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -20,3 +20,52 @@ void freeArray(char **arr)
 	};
 	free(arr), arr = NULL;
 }
+
+void error(char *name, char *cmd, int indexnum)
+{
+	char *index, message[] = ": not found\n";
+	index = _inttoascii();
+	write(STDERR_FILENO, name, _strlen(name));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, index, _strlen(index));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, cmd, _strlen(cmd));
+	write(STDERR_FILENO, message, _strlen(message));
+
+	free (index);
+}
+char *_inttoascii()
+{
+    char buffer[25];
+    int i = 0;
+
+    if (n == 0)
+        buffer[i++] = '0';
+    else
+    {
+        while (n > 0)
+        {
+            buffer[i++] = (n % 10) + '0';
+            n /= 0;
+        }
+    }
+    buffer[i] = '0';
+    reversestr(buffer, i);
+
+    return (strdup(buffer));
+}
+void revrsestr(char *str, int len)
+{
+    char tmp;
+    int start = 0;
+    int end = len - 1;
+
+    while (start < end)
+    {
+        tmp = str[start];
+        str[start] = str[end];
+        str[end] = tmp;
+        start++;
+        end--;
+    }
+}
